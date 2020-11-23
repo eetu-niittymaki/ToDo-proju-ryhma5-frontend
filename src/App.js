bb1506d8ab8f778f9e1481070eacf7b1778cf4bc
Jussi <jussi.kanto@tuni.fi>
2020-11-23 21:58:56 +0200

    Add modification to addTodo method

--- src/App.js
+++ src/App.js
@@ -1,80 +1,91 @@
-import './App.css';
-import React, { Component } from 'react'
-import Todos from './components/Todos.js'
-import AddTodo from './components/AddTodo.js'
-import Header from './components/layout/Header.js'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import About from './components/pages/About.js'
+import "./App.css";
+import React, { Component } from "react";
+import Todos from "./components/Todos.js";
+import AddTodo from "./components/AddTodo.js";
+import Header from "./components/layout/Header.js";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import About from "./components/pages/About.js";
 
-let id = 1
+let id = 1;
 
 class App extends Component {
   state = {
     todos: [
       {
         id: id++,
-        title: 'Do homework',
+        title: "Do homework",
         priority: 1,
-        dueDate: '20-11-2020',
-        completed: false
+        dueDate: "20-11-2020",
+        completed: false,
       },
       {
         id: id++,
-        title: 'Sleep, dream',
+        title: "Sleep, dream",
         priority: 4,
-        dueDate: '20-11-2020',
-        completed: false
+        dueDate: "20-11-2020",
+        completed: false,
       },
       {
         id: id++,
-        title: 'Take out the garbage',
+        title: "Take out the garbage",
         priority: 5,
-        dueDate: '20-11-2020',
-        completed: false
-      }
-    ]
-  }
+        dueDate: "20-11-2020",
+        completed: false,
+      },
+    ],
+  };
 
   // Toggle Complete
   markComplete = (id) => {
-    this.setState({ todos: this.state.todos.map(todo => {
-      if(todo.id === id) {
-        todo.completed = !todo.completed
-      }
-      return todo
-    })})
-  }
-  
+    this.setState({
+      todos: this.state.todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      }),
+    });
+  };
+
   // Delete ToDo
   delTodo = (id) => {
-    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] })
-  }
+    this.setState({
+      todos: [...this.state.todos.filter((todo) => todo.id !== id)],
+    });
+  };
 
   // Add Todo
-  addTodo = (title, priority, dueDate) => {
+  addTodo = (todo) => {
     const newTodo = {
       id: id++,
-      title: title,
-      priority: priority,
-      dueDate: dueDate,
-      completed: false
-    }
-    this.setState({ todos: [...this.state.todos, newTodo] })
-  }
+      title: todo[0],
+      priority: todo[1],
+      dueDate: todo[2],
+      completed: false,
+    };
+    this.setState({ todos: [...this.state.todos, newTodo] });
+  };
 
   render() {
     return (
       <Router>
         <div className="App">
           <div className="container">
-            <Header/>
-            <Route exact path="/" render={props => (
-              <React.Fragment>
-                <AddTodo addTodo={this.addTodo}/>
-                <Todos todos={this.state.todos} markComplete={this.markComplete}
-                  delTodo={this.delTodo}/>
-              </React.Fragment>
-            )} />
+            <Header />
+            <Route
+              exact
+              path="/"
+              render={(props) => (
+                <React.Fragment>
+                  <AddTodo addTodo={this.addTodo} />
+                  <Todos
+                    todos={this.state.todos}
+                    markComplete={this.markComplete}
+                    delTodo={this.delTodo}
+                  />
+                </React.Fragment>
+              )}
+            />
             <Route path="/about" component={About} />
           </div>
         </div>
