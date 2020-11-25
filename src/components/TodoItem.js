0f02af3bc948fe1a0868c281b7b9835948167712
Jussi <jussi.kanto@tuni.fi>
2020-11-25 12:25:07 +0200

    Change css to sass

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import "../App.css";
+import "../App.sass";
 
 export class TodoItem extends Component {
   state = { title: "", priority: "", dueDate: "" };
