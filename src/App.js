006508649f03d06a2ca0f0d676e3b175ecf7a125
Eetu <eetu.niittymaki@tuni.fi>
2020-12-09 18:34:01 +0200

    Add sorting feature

--- src/App.js
+++ src/App.js
@@ -15,13 +15,16 @@ class App extends Component {
     super(props)
     this.state = {
       todos: [],
+      sortTask: true,
+      sortPriority: true,
+      sortDueDate: true,
       port: (process.env.PORT || 8080)
     }
   }
 
   // GET all
   async componentDidMount() {
-    let hr = await fetch(`http://localhost:${this.state.port}/todos/`)
+    let hr = await fetch(`http://localhost:${this.state.port}/todos?sort=timestamp&order_by=asc`)
     let json = await hr.json()
     this.setState({ todos: json })
   }
@@ -57,8 +60,31 @@ class App extends Component {
     }
     this.setState({ todos: [...this.state.todos, newTodo] }) 
   }
-  
-  
+
+  // Sort by Task
+  sortTask = async () => {
+    let method = (this.state.sortTask) ? "asc" : "desc"
+    let hr = await fetch(`http://localhost:${this.state.port}/todos?sort=task&order_by=${method}`)
+    let json = await hr.json()
+    this.setState({ todos: json, sortTask: !this.state.sortTask })
+  }
+
+  // Sort by Priority
+  sortPriority = async () => {
+    let method = (this.state.sortPriority) ? "asc" : "desc"
+    let hr = await fetch(`http://localhost:${this.state.port}/todos?sort=priority&order_by=${method}`)
+    let json = await hr.json()
+    this.setState({ todos: json, sortPriority: !this.state.sortPriority })
+  }
+
+  // Sort by Due Date
+  sortDueDate = async () => {
+    let method = (this.state.sortDueDate) ? "asc" : "desc"
+    let hr = await fetch(`http://localhost:${this.state.port}/todos?sort=due_date&order_by=${method}`)
+    let json = await hr.json()
+    this.setState({ todos: json, sortDueDate: !this.state.sortDueDate })
+  }
+
   render() {
     return (
       <Router>
@@ -67,6 +93,29 @@ class App extends Component {
           <video src='/videos/video-1.mp4' autoPlay loop muted /> 
             <SoundEffect />
             <Header/>
+            <div className="sortContainer">Sort By: 
+              <button 
+                type="button"
+                className="sortBtn"
+                value="Submit"
+                onClick={this.sortTask}
+              > Task 
+              </button>
+              <button 
+                type="button"
+                className="sortBtn"
+                value="Submit"
+                onClick={this.sortPriority}
+              > Priority 
+              </button>
+              <button 
+               type="button"
+                className="sortBtn"
+                value="Submit"
+                onClick={this.sortDueDate}
+              > Due Date 
+              </button>
+            </div>
             <Route exact path="/" render={props => (
               <React.Fragment>
                 <AddTask addTask={this.addTask}/>
@@ -80,7 +129,7 @@ class App extends Component {
           </div>
         </div>
       </Router>
-    );
+    )
   }
 }
 
